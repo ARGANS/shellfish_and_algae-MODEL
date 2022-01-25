9a0238c7bb520b50bf13d40ccf0887314c2bfec0
martin <martwine@gmail.com>
2022-01-25 10:32:46 +0000

    new version of Hadley model with working light limitation scheme from Zollmann et al 2021

--- macroalgae/hadley_model_test.R
+++ macroalgae/hadley_model_test.R
@@ -3,12 +3,12 @@
 #### load libraries
 
 library('deSolve')
-library('rootSolve')
-library('bvpSolve')
-library('deTestSet')
-library('ReacTran')
-library('simecol')
-library('ggplot2')
+#library('rootSolve')
+#library('bvpSolve')
+#library('deTestSet')
+#library('ReacTran')
+#library('simecol')
+#library('ggplot2')
 
 #### create harvesting data frame ######
 EST <- 30 #establishment time /d
@@ -34,10 +34,11 @@ hadley_harvest_function<-function(EST=EST,HAR=HAR,HF=HF,name,run_length) {
 }
 
 
-harvest_regime<-rbind(
-  harvest_function(EST,HAR,HF,name='N_s'),
-  harvest_function(EST,HAR,HF,name='N_f')
-  )
+#harvest_regime<-rbind(
+#  hadley_harvest_function(EST,HAR,HF,name='N_s'),
+#  hadley_harvest_function(EST,HAR,HF,name='N_f')
+#
+#)
 
 #### model boundary forcings ###########################################
 
@@ -77,7 +78,26 @@ D_ref  <- 0.1  #simply set to constant for now
 
 
 
-parms <- c(
+parms_ulva <- c(
+  mu      = 0.45,    # maximum growth rate           / 1/d
+  V_NH4   = 124,      # max. ammonium uptake rate     / mg(N)g-1(dw)d-1
+  V_NO3   = 39,      # max. nitrate uptake rate      / mg(N)g-1(dw)d-1
+  K_NH4   = 700,     # Half saturation constant NH4  / mg(N)m-3
+  K_NO3   = 70,     #   "      "          "    NO3  / mg(N)m-3
+  Q_max   = 42,      # max. internal nitrogen        / mg(N) g-1 (dw)
+  Q_min   = 13,      # min.    "        "            / mg(N) g-1 (dw)
+  K_c     = 7,       # Half growth constant          / mg(N) g-1 (dw)
+  T_O     = 12,      # optimum growth temperature    / oC
+  T_r     = 1,       # range of optimum temperature  / oC
+  I_s     = 200,     # saturation irradiance         / umol photons m-2 s-1
+  a_cs    = 0.00033, # nitrogen-specific shading     / m2 mg-1 (N)
+  d_m     = 0.003,   # mortality rate                / 1/d
+  h_MA    = 0.2,     # height of seaweed             / m
+  r_L     = 0.2,     # remineralisation rate         / 1/d
+  r_N     = 0.1     # nitrification rate            / 1/d
+)
+
+parms_porphyra <- c(
   mu      = 0.33,    # maximum growth rate           / 1/d
   V_NH4   = 60,      # max. ammonium uptake rate     / mg(N)g-1(dw)d-1
   V_NO3   = 25,      # max. nitrate uptake rate      / mg(N)g-1(dw)d-1
@@ -93,11 +113,15 @@ parms <- c(
   d_m     = 0.003,   # mortality rate                / 1/d
   h_MA    = 0.2,     # height of seaweed             / m
   r_L     = 0.2,     # remineralisation rate         / 1/d
-  r_N     = 0.1,     # nitrification rate            / 1/d
+  r_N     = 0.1     # nitrification rate            / 1/d
+)
+
+parms_farm<-c(
   K_d     = 0.1,     # light attenuation coefficient / 1/m
   A_farm  = 1,       # farm area                     / m2
   z       = 3,       # cultivation depth             / m
-  F_in    = 3     # flow rate into farm           / m3 d-1
+  F_in    = 0.75,     # flow rate into farm           / m3 d-1
+  N_farm  = 5000# additional ammonium input to farm e.g. from salmon mg/N/m-3
 )
 
 ######################################################
@@ -106,23 +130,84 @@ parms <- c(
 Hadley_model <- function(t, y, parms) {
 
   with(as.list(c(y, parms)), {
+    length_scaler<-1
+    #length_scaler<-max(h_MA/z,1)
+    #length_scaler<-min(h_MA/z,1)
+
     lambda_R    <- F_in/(A_farm*z)                                             # refresh rate of farm
     V_MA        <- h_MA * A_farm                                             # volume of macroalgae
     Q           <- Q_min*(1+(N_s/N_f))                                       # Internal nutrient quota of macroalgae
     B           <- N_f/Q_min                                                 # Biomass of dry macroalgae
-    K_MA        <- N_f*a_cs*(max(h_MA/z,1)*(min(h_MA,z)^(-1)))               # light attenuation due to macroalgae
+    #K_MA        <- N_f*a_cs*max(h_MA/z,1)*(min(h_MA,z)^(-1))               # light attenuation due to macroalgae
+    #K_MA        <- N_f*a_cs                                                  # height scaling removed due to making no sense and not being relevant to fixed heigh MA model
+    #K           <- K_MA + K_d                                                # total light attenuation due to water and algae
+    #E_z         <- PAR(t)*exp(-K*z)                                          # Irradiance at top of macroalgal canopy
+    g_Q         <- (Q-Q_min)/(Q-K_c)                                         # Growth limitation due to internal nutrient reserves
+    g_T         <- 1/(1+exp(-(SST(t)-T_O)/T_r))                              # Growth limitation due to temperature
+    #g_E         <- (PAR(t)/(K*h_MA))*(exp(-(E_z*exp(-K*h_MA))/I_s)-exp(-(E_z/I_s)))# Growth limitation due to light
+
+   # new light limitation - from Zollman et al 2021
+    I_top<-PAR(t)*exp(-K_d*(z-h_MA))                                    # calculate incident irradiance at the top of the farm
+    I_av <-(I_top/(K_d*z+N_f*a_cs))*(1-exp(-(K_d*z+N_f*a_cs)))          # calclulate the average irradiance throughout theheight of the farm
+    g_E <- I_av/(((I_s)+I_av))                                          # light limitation scaling function
+
+     mu_g_EQT    <- mu*g_E*g_Q*g_T                                            # Growth function for macroalgae
+    f_NH4       <- ((V_NH4*NH4)/(K_NH4+NH4))*((Q_max-Q)/(Q_max-Q_min))          # uptake rate of NH4
+    f_NO3       <- ((V_NO3*NO3)/(K_NO3+NO3))*((Q_max-Q)/(Q_max-Q_min))          # uptake rate of NO3.
+
+    dNH4        <- lambda_R*(NH4_ref(t)-NH4)-(f_NH4*B*length_scaler)+(r_L*D)-(r_N*NH4)+(d_m*N_s)+N_farm    # change in NH4 with time  - eq 3 in Hadley et al (note max(h_MA/z,1) term is omitted because we assume the surface box is well mixed - need to think further about this - should we be looking across entire mixed layer - or include this in the lambda term?)
+    dNO3        <- lambda_R*(NO3_ref(t)-NO3)-(f_NO3*B*length_scaler)+(r_N*NH4)             # change in NO3 with time  - eq 4 in Hadley et al (note max(h_MA/z,1) term is omitted because we assume the surface box is well mixed - need to think further about this - should we be looking across entire mixed layer - or include this in the lambda term?)
+
+    dN_s        <- (f_NH4+f_NO3)*B*length_scaler-mu_g_EQT*N_s-(d_m*N_s)                          # change in internal nitrogen store - eq 5 in Hadley
+
+
+         dN_f        <- mu_g_EQT*N_s-(d_m*N_f)                                    # change in fixed nitrogen (i.e. biomass nitrogen) - eq 6 in Hadley
+    dD          <- lambda_R*(D_ref-D) + d_m*N_f - r_L*D                      # change in detritus with time
+    list(c(dNH4, dNO3,dN_s,dN_f,dD),
+         lambda_R  = lambda_R,
+         V_MA      = V_MA,
+         Q         = Q,
+         B         = B,
+         #K_MA      = K_MA,
+         #K         = K,
+         #E_z       = E_z,
+         I_av     = I_av,
+         g_Q       = g_Q,
+         g_T       = g_T,
+         g_E       = g_E,
+         mu_g_EQT  = mu_g_EQT,
+         f_NH4     = f_NH4,
+         f_NO3     = f_NO3
+         )
+  })
+}
+
+
+Hadley_model_as_published <- function(t, y, parms) {
+
+  with(as.list(c(y, parms)), {
+    length_scaler<-max(hma/z,1)
+    lambda_R    <- F_in/(A_farm*z)                                             # refresh rate of farm
+    V_MA        <- h_MA * A_farm                                             # volume of macroalgae
+    Q           <- Q_min*(1+(N_s/N_f))                                       # Internal nutrient quota of macroalgae
+    B           <- N_f/Q_min                                                 # Biomass of dry macroalgae
+    K_MA        <- N_f*a_cs*length_scaler/(min(h_MA,z))               # light attenuation due to macroalgae
     K           <- K_MA + K_d                                                # total light attenuation due to water and algae
     E_z         <- PAR(t)*exp(-K*z)                                          # Irradiance at top of macroalgal canopy
     g_Q         <- (Q-Q_min)/(Q-K_c)                                         # Growth limitation due to internal nutrient reserves
     g_T         <- 1/(1+exp(-(SST(t)-T_O)/T_r))                              # Growth limitation due to temperature
+
     g_E         <- (exp(1)/(K*h_MA))*(exp(-(E_z*exp(-K*h_MA))/I_s)-exp(-(E_z/I_s)))# Growth limitation due to light
     mu_g_EQT    <- mu*g_E*g_Q*g_T                                            # Growth function for macroalgae
     f_NH4       <- ((V_NH4*NH4)/(K_NH4+NH4))*((Q_max-Q)/(Q_max-Q_min))          # uptake rate of NH4
     f_NO3       <- ((V_NO3*NO3)/(K_NO3+NO3))*((Q_max-Q)/(Q_max-Q_min))          # uptake rate of NO3.
-    dNH4        <- lambda_R*(NH4_ref(t)-NH4)-(f_NH4*B)+(r_L*D)-(r_N*NH4)+(d_m*N_s)     # change in NH4 with time  - eq 3 in Hadley et al (note max(h_MA/z,1) term is omitted because we assume the surface box is well mixed - need to think further about this - should we be looking across entire mixed layer - or include this in the lambda term?)
-    dNO3        <- lambda_R*(NO3_ref(t)-NO3)-(f_NO3*B)+(r_N*NH4)             # change in NO3 with time  - eq 4 in Hadley et al (note max(h_MA/z,1) term is omitted because we assume the surface box is well mixed - need to think further about this - should we be looking across entire mixed layer - or include this in the lambda term?)
 
-    dN_s        <- (f_NH4+f_NO3)*B-mu_g_EQT*N_s-(d_m*N_s)                          # change in internal nitrogen store - eq 5 in Hadley
+    dNH4        <- lambda_R*(NH4_ref(t)-NH4)-(f_NH4*B*length_scaler)+(r_L*D)-(r_N*NH4)+N_farm     # change in NH4 with time  - eq 3 in Hadley et al (note max(h_MA/z,1) term is omitted because we assume the surface box is well mixed - need to think further about this - should we be looking across entire mixed layer - or include this in the lambda term?)
+    dNO3        <- lambda_R*(NO3_ref(t)-NO3)-(f_NO3*B*length_scaler)+(r_N*NH4)             # change in NO3 with time  - eq 4 in Hadley et al (note max(h_MA/z,1) term is omitted because we assume the surface box is well mixed - need to think further about this - should we be looking across entire mixed layer - or include this in the lambda term?)
+
+    dN_s        <- (f_NH4+f_NO3)*B*length_scaler-mu_g_EQT*N_s-(d_m*N_s)                          # change in internal nitrogen store - eq 5 in Hadley
+
+
     dN_f        <- mu_g_EQT*N_s-(d_m*N_f)                                    # change in fixed nitrogen (i.e. biomass nitrogen) - eq 6 in Hadley
     dD          <- lambda_R*(D_ref-D) + d_m*N_f - r_L*D                      # change in detritus with time
     list(c(dNH4, dNO3,dN_s,dN_f,dD),
@@ -139,19 +224,77 @@ Hadley_model <- function(t, y, parms) {
          mu_g_EQT  = mu_g_EQT,
          f_NH4     = f_NH4,
          f_NO3     = f_NO3
-         )
+    )
   })
 }
 
-y0   <- c(NH4=NH4_ref(1),NO3=NO3_ref(1),N_s=0.01,N_f=0.01,D=0.1)
+y0   <- c(NH4=NH4_ref(1),NO3=NO3_ref(1),N_s=0.01,N_f=1,D=0.1)
 start_time<-Sys.time()
- Out_Harvets <- ode(times = times, func = Hadley_model, y = y0, parms = parms, event=list(data=harvest_regime))
- Out <- ode(times = times, func = Hadley_model, y = y0, parms = parms)
+#Out_Harvets <- ode(times = times, func = Hadley_model, y = y0, parms = parms, event=list(data=harvest_regime))
+Out <- ode(times = times, func = Hadley_model, y = y0, parms = c(parms_porphyra,parms_farm))
  end_time<-Sys.time()
 
-print(end_time-start_time)
+#print(end_time-start_time)
 
-plot(Out)
+#plot(Out)
+
+#### investigating Hadley model functions
+
+
+light_limitation<-function(N_f=30000,
+                           a_cs=parms_porphyra['a_cs'],
+                           I_s=parms_porphyra['I_s'],
+                           h_MA=parms_porphyra['h_MA'],
+                           K_d=parms_farm['K_d'],
+                           z=parms_farm['z'],
+                           PAR.=PAR(91)){
+
+
+    #print(paste("N_f:",N_f))
+    #print(paste("a_cs:",a_cs))
+    #print(paste("z:",z))
+    #print(paste("h_MA:",h_MA))
+    #print(paste("I_s:", I_s))
+    #print(paste("K_d:", K_d))
+    #print(paste("PAR:", PAR.))
+
+
+    K_MA        <- N_f*a_cs*max(h_MA/z,1)/(min(h_MA,z))
+
+    #print(paste("K_MA:",K_MA))
+
+    K           <- K_MA + K_d
+
+    #print(paste("K", K))
+
+    E_z         <- PAR.*exp(-K*z)
+
+    #print(paste("E_z:", E_z))
+
+    g_e<-(exp(1)/(K*h_MA))*(exp(-(E_z*exp(-K*h_MA))/I_s)-exp(-(E_z/I_s)))# Growth limitation due to light
+
+    #print(paste("g_e:", g_e))
+    g_e
+
+}
+
+
+
+light_limitation_zollmann<-function(N_f=30000,
+                                     a_cs=parms_porphyra['a_cs'],
+                                     I_s=parms_porphyra['I_s'],
+                                     h_MA=parms_porphyra['h_MA'],
+                                     K_d=parms_farm['K_d'],
+                                     z=parms_farm['z'],
+                                     PAR.=PAR(91)){
+  
+  I_top<-PAR.*exp(-K_d*(z-h_MA))
+  I_av <-(I_top/(K_d*h_MA+N_f*a_cs))*(1-exp(-(K_d*h_MA+N_f*a_cs)))
+  g_E <- I_av/((I_s+I_av))
+  g_E
+  
+}
+
 
 
 
