bf051be999c1f292715b30e929d287ee1af25883
Quentin Jutard <qjutard@argans.eu>
2022-03-10 15:59:47 +0100

    encapsulation works with latest versions of the model and the json params

--- dataread/launch_model.py
+++ dataread/launch_model.py
@@ -9,16 +9,16 @@ import json
 
 if __name__ =="__main__":
 
-    with open('P:/Aquaculture/shellfish_and_algae-MODEL/macroalgae/run_MA.R', 'r') as f:
+    with open('P:/Aquaculture/shellfish_and_algae-MODEL/macroalgae/macroalgae_model.R', 'r') as f:
         body = f.read()
-        run_MA = STAP(body, "run_model")
+        model = STAP(body, "run_MA_model")
 
     # Read from CSV for now
-    bantryData = pd.read_csv('P:/Aquaculture/shellfish_and_algae-MODEL/macroalgae/bantry_data/bantry_MLDaveraged.csv', delimiter=';')
+    bantryData = pd.read_csv('P:/Aquaculture/shellfish_and_algae-MODEL/macroalgae/bantry_data/bantry_3m.csv', delimiter=';')
     bantryData['date'] = [datetime.datetime.fromisoformat(str_date) for str_date in bantryData['date']]
 
     dataToR = pd.DataFrame({
-        'time': [(date - bantryData['date'][0]).days for date in bantryData['date']],
+        'time': [(date - bantryData['date'][0]).days + 1 for date in bantryData['date']],
         'SST': 15,
         'PAR': bantryData['par'],
         'NH4_in': bantryData['Ammonium'],
@@ -31,41 +31,35 @@ if __name__ =="__main__":
         'D_in': 0.1
     })
 
-    print(dataToR)
-
     with open('P:/Aquaculture/shellfish_and_algae-MODEL/macroalgae/model_parameters.json', 'r') as f:
         default_parms = json.loads(f.read())
 
+    param_lists = [robjects.ListVector({"latitude": 55.})]
+    for _,section in default_parms.items():
+        # just take the first option for each section for now
+        first_default = section['defaults'][next(iter(section['defaults']))]
 
-    parms_species = default_parms['algae']['species']['ulva']['parameters']
-    parms_farm = default_parms['algae']['farm_parameters']
-    parms_run = default_parms['algae']['run_parameters']
-
-    parms_run['light_scheme'] = 2
-
-    parms_species = robjects.ListVector(parms_species)
-    parms_farm = robjects.ListVector(parms_farm)
-    parms_run = robjects.ListVector(parms_run)
-    parms_additional = robjects.ListVector({"latitude": 55.})
+        param_lists.append(robjects.ListVector(first_default['parameters']))
+        param_lists.append(robjects.ListVector(first_default['options']))
 
     # get the R concatenator function
     conc = robjects.r('c')
 
-    parameters = conc(parms_species, parms_farm, parms_run, parms_additional)
+    # concatenate all parameter lists
+    parameters = conc(*param_lists)
 
-    y0 = robjects.ListVector({"NH4": 0,
-                              "NO3": 0,
-                              "N_s": 1,
-                              "N_f": 1,
-                              "D": 0,
-                              "Yield": 0
-                            })
+    y0 = robjects.r('c("NH4" = 0, \
+                       "NO3" = 0, \
+                       "N_s" = 1, \
+                       "N_f" = 1, \
+                       "D" = 0, \
+                       "Yield" = 0,\
+                       "Yield_per_m" = 0)')
 
-    #print(parms)
 
     # Using a converter between R data.frame and python pd.DataFrame
     with localconverter(robjects.default_converter + pandas2ri.converter):
-       out = run_MA.run_model(input=dataToR, parameters=parameters, y0=y0)
+       out = model.run_MA_model(input=dataToR, parameters=parameters, y0=y0)
 
     print(out)
 
