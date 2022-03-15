82c9906dc857227ae7960bfaf16eecb0d311c3a0
Quentin Jutard <qjutard@argans.eu>
2022-03-15 14:39:04 +0100

    attempt to define the pretreatment container

---
+++ manage_pretreatment.sh
@@ -0,0 +1,43 @@
+DIR='pretreatment'
+DD_TAG='aquaculture/pretreatment'
+DOCKERFILE="./$DIR/pretreatment.Dockerfile"
+CONTAINER_NAME='NCOcontainer'
+
+function run {
+    local command="$1"
+
+    case $command in
+        'build')
+            docker build \
+                --network host \
+                -t $DD_TAG:v1 -t $DD_TAG:latest \
+                -f $DOCKERFILE ./$DIR
+            ;;
+        'run')
+            container_id=$( docker ps -q -f name=$CONTAINER_NAME )
+            
+            if [[ ! -z "$container_id" ]]; then
+                docker stop $CONTAINER_NAME || true
+            fi
+
+            image_id=$( docker images -q $DD_TAG )
+
+            if [[ -z "$image_id" ]]; then
+                run "build"
+            fi
+
+            docker run \
+                --rm \
+                --name $CONTAINER_NAME \
+                --volume "$PWD/share":/media/share \
+                --entrypoint '/usr/local/bin/python' \
+                -dit $DD_TAG:latest
+
+            docker exec -it $CONTAINER_NAME bash 
+            ;;
+        *)
+            echo 'todo';;
+    esac
+}
+
+run "$1"
