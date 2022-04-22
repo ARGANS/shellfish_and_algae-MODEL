cdf1a21c69c2e68db9d37ace40b0a72bc4610a0b
Nikolai Maltsev <nmaltsev@argans.eu>
2022-04-22 18:38:32 +0200

    Minor improvements

--- pretreatment/src/concatenate_copernicus.sh
+++ pretreatment/src/concatenate_copernicus.sh
@@ -15,15 +15,12 @@ fi
 first_file_name=$(ls $DIR_IN/* | head -n 1 | xargs -n 1 basename)
 echo "first_file_name: $first_file_name"
 
-echo "On first file, make 'time' into the record dimension:"
 # On first file, make 'time' into the record dimension:
 ncks -O --mk_rec_dim time $DIR_IN/$first_file_name $DIR_OUT/$first_file_name
 
-echo "Then concatenate all files (the modified first file, then all the unmodified files):"
 # Then concatenate all files (the modified first file, then all the unmodified files):
 shopt -s extglob
 ncrcat -h $DIR_OUT/$first_file_name $DIR_IN/!($first_file_name) $DIR_OUT/${ZONE}_${PARAM}_merged.nc
 
-echo "Finally change 'valid_max' attribute to a large value:"
 # Finally change 'valid_max' attribute to a large value:
 ncatted -O -a valid_max,time,o,d,999999. $DIR_OUT/${ZONE}_${PARAM}_merged.nc
