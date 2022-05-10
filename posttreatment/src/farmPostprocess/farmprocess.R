1f05d97d9531b3fcef8879693a7753d9947e435f
martin <martwine@gmail.com>
2022-05-10 17:12:10 +0100

    first simple functions to quantify parameters of interest

--- posttreatment/src/farmPostprocess/farmprocess.R
+++ posttreatment/src/farmPostprocess/farmprocess.R
@@ -24,13 +24,35 @@ getRunParms<-function(parms_json_file){
   return(c(type=type,species=species,parmsJSON$parameters$farm$default$parameters))
 }
 
-farmPostProcess<-function(data,parms_json_file)
+farmPostProcess_protein<-function(data){
+ #return the protein content yield in kg per m2 across the whole farm area
+  data$protein_PUA[nrow(data)]
+}
 
+farmPostProcess_kcal<-function(data){
+  #return the energy yield in kcal per m2 across the whole farm area
+  data$kcal_PUA[nrow(data)]
+}
 
+farmPostProcess_CO2<-function(data){
+  #simple estimate of farm CO2 uptake in tonnes CO2 per farm - need refining to account for buffering by carbonate system
+  tonnesC<-cumsum(data$Farm_CO2_demand)[nrow(data)]/1e6
+  tonnesC*3.67
+}
 
+farmPostProcess_DW_yield<-function(data,json_file){
+  #DW yield in kg/m2
+  parms<-getRunParms(json_file)
+  if(parms$type=='macroalgae'){
+    data$B_vol*parms$h_ma
+  }
+    else if(parms$type=='shellfish'){
+      
+  } else stop ('could not parse farm type in farmPostProcess function')
+}
 
-test_parms_json_file<-'../../macroalgae/macroalgae_model_parameters_input.json'
-test_data<-read.csv('../../macroalgae/bantry_data/bantry_run.csv',header=TRUE)
+test_parms_json_file<-'../../../macroalgae/macroalgae_model_parameters_input.json'
+test_data<-read.csv('../../../macroalgae/bantry_data/bantry_run.csv',header=TRUE)
 
 parmsJSON<-fromJSON(file=test_parms_json_file)
 
