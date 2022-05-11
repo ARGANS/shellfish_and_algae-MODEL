add3453c982ed1dd93a7234fdb78ad059bd2a6bb
mjaouen <mjaouen@argans.eu>
2022-05-11 12:26:12 +0200

    changed the code to download pCO2, disolved_inorganic_carbon and primary_producer_POC

--- dataimport/src/main.py
+++ dataimport/src/main.py
@@ -2,14 +2,14 @@ from general import readcsv, giveDateslist, getData
 import pandas as pd
 import os
 
-wantedData=['par']
+wantedData=['pCO2','disolved_inorganic_carbon','primary_producer_POC']
 dateBeginning = '2021-01-01 00:00:00'
 dateEnd = '2022-01-01 00:00:00'
 zone='IBI'
 deepthmin=0
 deepthmax=20
 outputDirectory = os.getenv('OUTPUT_DIR')
-frequency = 2
+frequency = 1
 
 #outputDirectory = 'I:/work-he/apps/safi/data/IBI/'
 outputDirectory = '/media/share/data/IBI/'
@@ -18,13 +18,13 @@ dataFin=pd.read_csv('./dataCmd.csv',';')
 
 datesList=giveDateslist(dateBeginning,dateEnd,frequency)
 
+
 for dat in wantedData:
     DataLine = dataFin.loc[dataFin["Parameter"] == dat]
     if DataLine.iloc[0]["daily"] > 0:
-        DataOutputDirectory = outputDirectory + dat + '/'
-        getData(dat, zone, dataFin, deepthmin, deepthmax,  DataOutputDirectory, datesList[0], datesList[1],frequency)
+        for (dateBeg, dateE) in zip(datesList[0], datesList[1]):
+            DataOutputDirectory = outputDirectory + dat + '/'
+            getData(dat, zone, dataFin, deepthmin, deepthmax, DataOutputDirectory, dateBeg, dateE, frequency)
     else:
         DataOutputDirectory = outputDirectory + dat + '/'
         getData(dat, zone, dataFin, deepthmin, deepthmax, DataOutputDirectory)
-
-
