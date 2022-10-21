27e5267a9f5edd51041d8fd519e454d997628720
Quentin Jutard <qjutard@argans.eu>
2022-10-21 11:55:30 +0000

    now looping over all areas for Europe

--- dataread/start.py
+++ dataread/start.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import datetime
 import time
+import json
 import os
 import multiprocessing as mp
 from src.make_runs import *
@@ -22,10 +23,24 @@ if not model_properties.isDataDownloadTaskCompleted():
 
 full_json = model_properties.attrs
 
-dict_dataCmd = full_json['dataset_parameters']['datasets']
-dict_to_AllData = dataCmd_to_AllData(dict_dataCmd, model_properties.file_template)
+if full_json['metadata']['zone'] != "Europe":
 
-algaeData = AllData(dict_to_AllData)
+    dict_dataCmd = full_json['dataset_parameters']['datasets']
 
+    dict_to_AllData = dataCmd_to_AllData(dict_dataCmd, model_properties.file_template)
+    algaeData = AllData(dict_to_AllData)
 
-time_spent = run_simulation(f"{workdir}/concat.nc", full_json, algaeData)
+    time_spent = run_simulation(f"{workdir}/concat.nc", full_json, algaeData)
+
+else:
+    for area_name in ['IBI', 'NWS', 'MED', 'Baltic', 'BS', 'Arctic']:
+        with open(f'/media/share/reference_data/{area_name}/parameters.json') as f:
+            dict_dataCmd_area = json.load(f)['datasets']
+
+        dict_to_AllData = dataCmd_to_AllData(dict_dataCmd_area,
+                            '/media/share/reference_data/{Place}/_pretreated/{Parameter}/{Parameter}{Place}modelNetCDF2021-01to2022-01.nc')
+        algaeData = AllData(dict_to_AllData)
+
+        time_spent = run_simulation(f"{workdir}/concat_{area_name}.nc", full_json, algaeData)
+
+        print(f'AREA {area_name} IS DONE. TIME SPENT: {time_spent/60} minutes.')
